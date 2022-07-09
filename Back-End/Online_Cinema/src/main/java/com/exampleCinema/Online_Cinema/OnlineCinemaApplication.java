3e89807e3409d00555a999f029bf1854cc6ab013
rahmasaidy <suleimanrahma563@gmail.com>
2022-07-09 04:55:01 +0300

    WEB PROJECT

---
+++ Back-End/Online_Cinema/src/main/java/com/exampleCinema/Online_Cinema/OnlineCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.exampleCinema.Online_Cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCinemaApplication.class, args);
+	}
+
+}
---
+++ Back-End/Online_Cinema/src/test/java/com/exampleCinema/Online_Cinema/OnlineCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleCinema.Online_Cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
